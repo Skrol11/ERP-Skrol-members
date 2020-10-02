922b8fc1c11abd3934f96fa8c5cba8daf63b9bf5
Bartlomiej Owsiany <bartlomiej.owsiany@gmail.com>
2020-10-02 15:45:06 +0200

    terminal, maincntrler, util

--- ERP/view/terminal.py
+++ ERP/view/terminal.py
@@ -11,7 +11,12 @@ def print_menu(title, list_options):
         title (str): the title of the menu (first row)
         list_options (list): list of the menu options (listed starting from 1, 0th element goes to the end)
     """
-    pass
+    print(f"{title}:")
+
+    for i in range(1,len(list_options)):
+        print(f"({i}) {list_options[i]}")
+
+    print(f"(0) {list_options[0]}\n")    
 
 
 def print_message(message):
@@ -20,7 +25,7 @@ def print_message(message):
     Args:
         message: str - the message
     """
-    pass
+    print(message)
 
 
 def print_general_results(result, label):
@@ -29,7 +34,18 @@ def print_general_results(result, label):
     lists/tuples (like "@label: \n  @item1; @item2"), and dictionaries
     (like "@label \n  @key1: @value1; @key2: @value2")
     """
-    pass
+    if type(result) == int:
+        print(f"{label}: {result}")
+    elif type(result) == float:
+        result_formatted = '{:.2f}'.format(result)
+        print(f"{label}: {result_formatted}")
+    elif type(result) == list or type(result) == tuple:
+        print(f"{label}:")
+        print('; '.join(f'{e}' for e in result))
+    elif type(result) == dict:
+        print(f"{label}:")
+        print('; '.join(f'{k}: {v}' for k,v in result.items()))
+    print('')
 
 
 # /--------------------------------\
@@ -38,23 +54,38 @@ def print_general_results(result, label):
 # |   0    |  Bazooka   | portable |
 # |--------|------------|----------|
 # |   1    | Sidewinder | missile  |
-# \-----------------------------------/
-def print_table(table):
+# \--------------------------------/
+def print_table(table, headers):
     """Prints tabular data like above.
 
     Args:
         table: list of lists - the table to print out
     """
-    pass
+    cell_sizes = [16, 16, 32, 16, 16]
 
+    print('-'*(sum(cell_sizes) + (3*len(headers)+1)))
+    print('| ',end='')
+    for ind, element in enumerate(headers): 
+        print(element + ' '*(cell_sizes[ind]-len(element)), end =' | ')
+    print('')
+    print('-'*(sum(cell_sizes) + (3*len(headers)+1)))
 
+    for row in table:
+        
+        print('| ',end='')
+        for ind, element in enumerate(row): 
+            print(element + ' '*(cell_sizes[ind]-len(element)), end =' | ')
+        print('')
+        print('-'*(sum(cell_sizes) + (3*len(row)+1)))
+
+        
 def get_input(label):
     """Gets single string input from the user.
 
     Args:
         label: str - the label before the user prompt
     """
-    pass
+    return input(f"{label}: ") 
 
 
 def get_inputs(labels):
@@ -63,8 +94,12 @@ def get_inputs(labels):
     Args:
         labels: list - the list of the labels to be displayed before each prompt
     """
-    pass
+    input_list = []
+    for label in labels:
+        input_list.append(get_input(f"{label}")) # tutaj mozna rozpisać sprawdzenie inputow np. dla daty
 
+    return input_list
+        
 
 def print_error_message(message):
     """Prints an error message to the terminal.
@@ -72,4 +107,4 @@ def print_error_message(message):
     Args:
         message: str - the error message
     """
-    pass
+    print(f"Error: {message}")
